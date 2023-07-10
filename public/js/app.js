90e4a70149487465519828318964a089df24878a
Ashutosh Ukale <123993522+ashutoshukale@users.noreply.github.com>
2023-07-10 13:43:26 +0530

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,11 @@
 const view = document.querySelector(".view");
 const viewSvg = document.querySelector(".svg");
-const search = document.querySelector("svg");
 const viewMoreBox = document.querySelector(".info-card");
 const time = document.querySelector(".time");
 const date = document.querySelector(".date");
 const viewLess = document.querySelector(".less");
 
+console.log(viewSvg)
 let currTime = () => {
     let currDate = new Date();
     let localTime = currDate.toTimeString();
@@ -39,12 +39,12 @@ view.addEventListener("click", ()=>{
     }
 });
 
-viewSvg.addEventListener("click", ()=>{
+viewSvg.addEventListener("onclick", ()=>{
     viewSvg.classList.toggle("rotate");
     viewMoreBox.classList.toggle("visible");
     view.classList.toggle("less");
     if(view.classList.contains("less")){
-        view.innerHTML = "View Less";
+        view.innerHTML = "View Less ";
     } else{
         view.innerHTML = "View More";
     }
